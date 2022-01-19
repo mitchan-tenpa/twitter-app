4bd5065480495048de201140a2319a657ef76c5d
mitchan <hs0127sh@gmail.com>
2022-01-19 14:58:55 +0900

    ⚡️ TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: [],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  // content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
