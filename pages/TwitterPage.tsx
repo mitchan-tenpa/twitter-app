65ed1ded71716ff8a52dc0cb4a576c7762137dff
mitchan <hs0127sh@gmail.com>
2022-01-19 15:00:14 +0900

    ✨ ページの追加

---
+++ pages/TwitterPage.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Twitter = () => {
+  return(
+    <div>
+      <div className="text-lg">Twitter Page</div>
+      <Link href="/">戻る</Link>
+    </div>
+  );
+};
+
+export default Twitter;
