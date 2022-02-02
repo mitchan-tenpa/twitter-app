920797a2c14c9e081610555ad114aca9e7d798b7
mitchan <hs0127sh@gmail.com>
2022-02-02 10:46:44 +0900

    ✨ トグルスィッチの作成

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import ToggleSwitch from "../ToggleSwitch";
 
 const Header = () => {
   const {theme, setTheme} = useTheme();
@@ -8,15 +9,16 @@ const Header = () => {
   if(!mounted) return null;
   return(
     <header className="flex justify-center items-center h-10% border-b-2 border-green-500">
-        <span className="">ホーム</span>
-        <button 
-          className="py-2 px-6 text-white dark:text-black bg-black dark:bg-white"
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-          Toggle to 
-          {theme === "light" ? "Dark" : "Light"}
-        </button>
-      </header>
+    <span className="">ホーム</span>
+    <button 
+      className="py-2 px-6 text-white dark:text-black bg-black dark:bg-white"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      Toggle to 
+      {theme === "light" ? "Dark" : "Light"}
+    </button>
+    <ToggleSwitch />
+  </header>
   );
 };
 
