7b0aa9382d2aa4b17570602ca541d14605790061
mitchan <hs0127sh@gmail.com>
2022-02-21 14:57:05 +0900

    🐛 あり　認証できんけどコミット

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,21 +8,13 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store } from "../src/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const persistor = persistStore(store); //この記述消すとRedux DevToolsのStateが消える
   
   return (
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
-    </ThemeProvider>
-
-    // <Provider store={store}>
-    //   <PersistGate persistor={persistor}>
-    //     <ThemeProvider attribute='class'>
-    //       <Component {...pageProps} />
-    //     </ThemeProvider>
-    //   </PersistGate>
-    // </Provider>
+    <Provider store={store}>
+      <ThemeProvider attribute='class'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -4,18 +4,22 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "original-twitter-app.REACT_APP_FIREBASE_APIKEY",
-  authDomain: "original-twitter-app.REACT_APP_FIREBASE_DOMAIN",
-  databaseURL: "original-twitter-app.REACT_APP_FIREBASE_DATABASE",
-  projectId: "original-twitter-app.REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "original-twitter-app.REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "original-twitter-app.REACT_APP_FIREBASE_SENDER_ID",
-  appId: "original-twitter-app.REACT_APP_FIREBASE_APP_ID"
+  apiKey: "original-twitter-app.NEXT_PUBLIC_FIREBASE_SPIKEY",
+  authDomain: "original-twitter-app.NEXT_PUBLIC_FIREBASE_DOMAIN",
+  databaseURL: "original-twitter-app.NEXT_PUBLIC_FIREBASE_DATABASE",
+  projectId: "original-twitter-app.NEXT_PUBLIC_FIREBASE_PROJECT_ID",
+  storageBucket: "original-twitter-app.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "original-twitter-app.NEXT_PUBLIC_FIREBASE_SENDER_ID",
+  appId: "original-twitter-app.NEXT_PUBLIC_FIREBASE_APP_ID"
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const db = firebaseApp.firestore();
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// export const db = firebaseApp.firestore();
 export const auth = firebase.auth();
 export const storage = firebase.storage();
 export const provider = new firebase.auth.GoogleAuthProvider();
